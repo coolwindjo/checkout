2e0bfcc83e4d11fd13625780e3c66773cebefdb4
saowang <saowang@outlook.com>
2020-08-17 12:54:49 +0800

    Add JEKYLL_GITHUB_TOKEN

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 
 cp -f /Gemfile . && rm -f Gemfile.lock
 
-bundle exec jekyll build
+JEKYLL_GITHUB_TOKEN=${INPUT_TOKEN} bundle exec jekyll build
 
 bundle exec htmlproofer ./_site --check-html --allow-hash-href --disable-external --empty-alt-ignore
