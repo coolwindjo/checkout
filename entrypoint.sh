83130d0b623c8b0700f4d5f2f541f8e3eb96f69e
saowang <68011645+saowang@users.noreply.github.com>
2020-09-07 23:30:57 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ cp -f /Gemfile . && rm -f Gemfile.lock
 
 JEKYLL_GITHUB_TOKEN=${INPUT_TOKEN} bundle exec jekyll build
 
-bundle exec htmlproofer ./_site --check-html --allow-hash-href --disable-external --empty-alt-ignore
+bundle exec htmlproofer ./_site --check-html --disable-external
