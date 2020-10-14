6e969202aa015fb5c2f05e2f2683c1b2c4f2211c
coolwindjo <coolwind@hotmail.co.kr>
2020-10-14 13:58:52 +0900

    Update entrypoint.sh
    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ cp -f /Gemfile . && rm -f Gemfile.lock
 
 JEKYLL_GITHUB_TOKEN=${INPUT_TOKEN} bundle exec jekyll build
 
-bundle exec htmlproofer /lut/ --check-html --disable-external
+bundle exec htmlproofer ./lut/ --check-html --disable-external
