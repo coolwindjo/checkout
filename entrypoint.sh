9ba6122b3bf1c980a8378cf223b3139ea16f3959
coolwindjo <coolwind@hotmail.co.kr>
2020-10-14 14:07:45 +0900

    Update entrypoint.sh
    disable entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-set -e
+#set -e
 
-cp -f /Gemfile . && rm -f Gemfile.lock
+#cp -f /Gemfile . && rm -f Gemfile.lock
 
-JEKYLL_GITHUB_TOKEN=${INPUT_TOKEN} bundle exec jekyll build
+#JEKYLL_GITHUB_TOKEN=${INPUT_TOKEN} bundle exec jekyll build
 
-bundle exec htmlproofer ./lut/ --check-html --disable-external
+#bundle exec htmlproofer ./lut/ --check-html --disable-external
